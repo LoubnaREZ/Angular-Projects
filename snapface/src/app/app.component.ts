f3d860e4a8bfd79c991848b4cc6b8adb79c77068
Loubna REZOUGUE <lrezougue@valsoftware.com>
2024-09-14 16:37:11 +0200

    control flow block @for

--- snapface/src/app/app.component.ts
+++ snapface/src/app/app.component.ts
@@ -12,34 +12,32 @@ import { FaceSnap } from './Moddels/face-snap';
   styleUrl: './app.component.scss'
 })
 export class AppComponent implements OnInit {
-  mySnap!: FaceSnap;
-  myOtherSnap!: FaceSnap;
-  myLastSnap!: FaceSnap;
+  faceSnaps!: FaceSnap[];
 
   ngOnInit(): void {
-    this.mySnap = new FaceSnap(
-      "Destination de rêve", 
-      "Toujours au soleil", 
-      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTrZGCQ9jac_mKUSNpIZcWJEQFnZpgvgzi_SA&s", 
-      new Date(), 
-      0
-    );
-    this.mySnap.setLocation("à la plage");
-
-    this.myOtherSnap = new FaceSnap(
-      "Boisson de rêve", 
-      "Le plein de vitamines", 
-      "https://images.anaca3.com/wp-content/uploads/2018/05/combien-de-calories-dans-un-jus-de-fruits-de-la-passion-702x336.jpg", 
-      new Date(), 
-      0
-    );
-
-    this.myLastSnap = new FaceSnap(
-      "Des vacances parfaites", 
-      "On se sent bien", 
-      "https://images.goelia.com/sigo/images/menu/VP_28.jpg?v=15", 
-      new Date(), 
-      0
-    );
+    this.faceSnaps = [
+      new FaceSnap(
+        "Destination de rêve", 
+        "Toujours au soleil", 
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTrZGCQ9jac_mKUSNpIZcWJEQFnZpgvgzi_SA&s", 
+        new Date(), 
+        0
+      ),
+      new FaceSnap(
+        "Boisson de rêve", 
+        "Le plein de vitamines", 
+        "https://images.anaca3.com/wp-content/uploads/2018/05/combien-de-calories-dans-un-jus-de-fruits-de-la-passion-702x336.jpg", 
+        new Date(), 
+        0
+      ),
+      new FaceSnap(
+        "Des vacances parfaites", 
+        "On se sent bien", 
+        "https://images.goelia.com/sigo/images/menu/VP_28.jpg?v=15", 
+        new Date(), 
+        0
+      )
+    ];
+    this.faceSnaps[0].setLocation("à la plage");
   }
 }
