5ef938472bb8fbb2919c78cf1475c64c7ed3a56c
Loubna REZOUGUE <lrezougue@valsoftware.com>
2024-09-24 13:06:09 +0200

    utilisation des observables

--- snapface/src/app/app.component.ts
+++ snapface/src/app/app.component.ts
@@ -1,16 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { RouterOutlet } from '@angular/router';
+import { filter, interval, map, Observable, tap } from 'rxjs';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
     HeaderComponent,
-    RouterOutlet
+    RouterOutlet,
+    AsyncPipe
   ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent { 
+export class AppComponent implements OnInit  { 
+  interval$!: Observable<string>;
+
+  ngOnInit(): void {
+    this.interval$ = interval(1000).pipe(
+      filter(value => value % 3 === 0), 
+      map(value => value % 2 === 0 ? `${value} est pair !` : `${value} est impair !`),
+      tap(text => this.logger(text))
+    );
+  }
+
+  logger(text: string): void {
+    console.log(`Log : ${text}`);
+  }
 }
